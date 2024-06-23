636e3296295ca16b83407ae06294b107acfbb25d
seryozhaokhta <93487244+seryozhaokhta@users.noreply.github.com>
2024-06-23 10:19:42 +0400

    3

--- src/components/MapApp.vue
+++ src/components/MapApp.vue
@@ -3,9 +3,10 @@
 <template>
     <div class="map-container">
         <div class="map-mask">
-            <svg class="world-map" :viewBox="viewBox">
+            <svg class="world-map" :viewBox="viewBox"
+                :style="{ transform: `translate(${translateX}px, ${translateY}px)` }">
                 <!-- Используем переменную для фоновой карты -->
-                <image :href="worldMap" width="100%" height="100%" />
+                <image :href="worldMap" />
                 <!-- Используем переменную для иконок точек -->
                 <image v-for="point in processedPoints" :key="point.id" :href="pointIcon" :x="point.x" :y="point.y"
                     width="4" height="4" />
@@ -23,31 +24,43 @@ export default {
     name: 'MapApp',
     data() {
         return {
-            worldMap, // Добавляем изображение карты как переменную данных
-            pointIcon, // Иконка точки теперь также переменная данных
+            worldMap,
+            pointIcon,
             points: [],
-            viewBox: '0 0 1000 500', // Начальное значение viewBox
+            viewBoxX: 0, // Добавляем начальное значение X для viewBox
+            viewBoxY: 0, // Добавляем начальное значение Y для viewBox
+            viewBoxWidth: 1000, // Значения width и height оставляем без изменений
+            viewBoxHeight: 500,
         };
     },
     mounted() {
         this.adjustViewBoxForMobile();
         window.addEventListener('resize', this.adjustViewBoxForMobile);
     },
-    beforeUnmount() { // Здесь было изменено с beforeDestroy на beforeUnmount
+    beforeUnmount() {
         window.removeEventListener('resize', this.adjustViewBoxForMobile);
     },
     methods: {
         adjustViewBoxForMobile() {
             if (window.innerWidth <= 768) {
-                // Уменьшаем область viewBox для мобильных устройств
-                this.viewBox = '250 125 500 250';
+                this.viewBoxWidth = 500;
+                this.viewBoxHeight = 250;
             } else {
-                // Возвращаем к исходному значению для десктопов
-                this.viewBox = '0 0 1000 500';
+                this.viewBoxWidth = 1000;
+                this.viewBoxHeight = 500;
             }
+        },
+        moveMap(x, y) {
+            // Изменяем viewBoxX и viewBoxY для "перемещения" карты
+            this.viewBoxX += x;
+            this.viewBoxY += y;
         },
     },
     computed: {
+        viewBox() {
+            // Возвращаем строку viewBox для использования в шаблоне
+            return `${this.viewBoxX} ${this.viewBoxY} ${this.viewBoxWidth} ${this.viewBoxHeight}`;
+        },
         processedPoints() {
             return this.points;
         }
@@ -69,7 +82,7 @@ export default {
 
 .map-mask {
     max-width: 100%;
-    max-height: 100vh;
+    height: 100vh;
     overflow: hidden;
     position: relative;
     border-radius: 24px;
@@ -80,11 +93,9 @@ export default {
 
 .world-map {
     width: 100%;
-    height: auto;
-    scale: 2;
+    height: 400px;
     max-width: 900px;
     max-height: 450px;
-    /* Половина изначальной высоты viewBox, чтобы сохранить аспект */
 }
 
 @media (max-width: 768px) {
